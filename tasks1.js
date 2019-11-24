660ff0e5bdaf4b19504814f5ed71e63bb181cff4
wintega <57044091+wintega@users.noreply.github.com>
2019-11-25 00:14:33 +0300

    Made some changes

--- tasks1.js
+++ tasks1.js
@@ -1,98 +1,89 @@
-//1 задание
-var arr = [-3,2,5,6];
-arr.length=3;
-function arraySum(arr){
-var sum = 0;
-for(var i = 0; i < arr.length; i++){
-    if (i > 0){
-    sum += arr[i];
-    }
-}
-console.log('Summa polojitelnih =', sum);
+function print(x){
+    console.log(x);
 }
 
+var digits = [-3, 12, 34];
 
-//2 задание
-arraySum(arr);
-//Произведение
-var proizvedenie = arr.reduce(function(a,b){return a*b;}); 
-//Сумма
-var summa = arr.reduce(function(a,b){return a+b;}); 
-//
-var z = Number (proizvedenie) + Number (summa) + 3;
-console.log('Summa + Proizvedenie + 3 =', z);
+//#1 задание - Найти сумму только положительных из трех чисел______________________
+let a = digits[0];
+let b = digits[1];
+let c = digits[2];
 
-//3 Задание
-console.log('Min-arr:', Math.min(...arr));
-console.log('Min-arr:',Math.max(...arr));
+let n = 0;
 
-var min = Math.min(...arr);
-var max = Math.max(...arr);
-var imin = arr.indexOf(min);
-var imax = arr.indexOf(max);
+if (a > 0){
+    n += a;
+}
+if (b > 0){
+    n += b;
+}
+if (c > 0){
+    n += c;
+} 
+print(n); //результат 46
 
-console.log('Index-min =', imin);
-console.log('Index-max =', imax);
+//#2 задание Посчитать выражение: макс(а*б*с, а+б+с) + 3 __________________________________
+let x = (a*b*c);
+let y = (a+b+c);
 
-//4 Задание
+if (x > y){
+    n = (x + 3);
+} else{
+    n = (y + 3);
+}
+print(n); //результат 46 
 
 
 
-let x = -3;
-let y = 5;
-
+//#3 задание - Определить, какой четверти принадлежит точка с координатами (х,у)___________________________
 if (x > 0 && y > 0){
-    console.log('1 4etvert');
+    print('1 4etvert');
 }
 if (x < 0 && y > 0){
-    console.log('2 4etvert');
+    print('2 4etvert');
 }
-
 if (x < 0 && y < 0){
-    console.log('3 4etvert');
+    print('3 4etvert');
 }
-
 if (x > 0 && y < 0){
-    console.log('4 4etvert');
+    print('4 4etvert');
 }
+// Результат: Выводит 2 четверть (x = - 1224, y = 43)
 
 
-
-
-
-//5 Задание
-function summ (max) {
-    function rand (max) {
-        return Math.round(Math.random() * (max));   
-    }
-    var mas = [],
-        sum = 0;
-    for (var i = 0; i < max; i++) {
-        mas[i] = rand(max);
-        ((i % 2) !== 0) ? sum += mas[i] : '';
-    }
-    console.log('Massiv: ' + mas);
-    console.log('Summa nechetnih: ' + sum);
+//#4 задание min max index (min, max)
+let max = 0;
+let min = 0;
+for (var i = 0; i < digits.length; i++) { 
+    if (max < digits[i])  max = digits[i];
 }
-summ(10);
+for (var i = 0; i < digits.length; i++) { 
+    if (min > digits[i])  min = digits[i];
+}
+print(min); // [-3, 12, 34] результат -3
+print(max); // [-3, 12, 34] результат 34
+
+print(imin = digits.indexOf(min)); // [-3, 12, 34] результат 0
+print(imax = digits.indexOf(max)); // [-3, 12, 34] результат 2
 
 
-var n=0;
+//5. Задание - Посчитать сумму элементов массива с нечетными индексами
+var mas = [2, 24, 25, 75, 46, 90, 23];
+sum = 0;
+for (var i = 0; i < mas.length; i++) {
+    if (i % 2 && i !==0) {
+        sum += mas[i];
+    }
+}
+print(sum); // результат 189 (24+75+90)
 
+//6. Найти сумму чисел от 1 до 99
+
+var sumOtr = 0;
 for (var i = 0; i < 99; i++) {
     if ((i % 2) === 0){
-    n += i
+    sumOtr += i;
     }
  }
 
- console.log('Summa chetnih ot 0-99:', n);
-
- var n=0;
-
-for (var i = 0; i < 99; i++) {
-    if ((i % 2) === 0){
-    n += 1
-    }
- }
-
- console.log('Kolichestvo chetnih ot 0-99:', n);
+ print(sumOtr); // результат 2450
